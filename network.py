661a1d14a6f1e320a41a0c86b25ea3f9b9364ef6
毛亚琛 <maoyachen55@gmail.com>
2021-11-22 18:17:44 +0800

    change the network

--- network.py
+++ network.py
@@ -1,36 +1,33 @@
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class Network(nn.Module):
     def __init__(self):
         super(Network, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=9, out_channels=32, kernel_size=(1, 9)),
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(1, 3)),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(1, 2), stride=2)
+            nn.MaxPool2d(kernel_size=(1, 2), stride=2),
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 9)),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 3)),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(1, 2), stride=2)
+            nn.MaxPool2d(kernel_size=(1, 2), stride=2),
         )
         self.fc1 = nn.Sequential(
-            nn.Linear(in_features=64 * 26, out_features=1000),
-            nn.ReLU()
+            nn.Linear(in_features=64 * 29, out_features=1000),
+            nn.ReLU(),
         )
         self.fc2 = nn.Sequential(
             nn.Linear(in_features=1000, out_features=500),
-            nn.ReLU()
+            nn.ReLU(),
         )
-        self.fc3 = nn.Sequential(
-            nn.Linear(in_features=500, out_features=6)
-        )
+        self.fc3 = nn.Sequential(nn.Linear(in_features=500, out_features=4))
 
     def forward(self, x):
         out = self.conv1(x)
         out = self.conv2(out)
-        out = out.reshape(-1, 64 * 26)
+        out = out.reshape(-1, 64 * 29)
         out = self.fc1(out)
         out = self.fc2(out)
         out = self.fc3(out)
